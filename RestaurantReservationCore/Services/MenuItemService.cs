026b941030306f71db8d826b3a2a9510b648a092
Amro <amroqadadha@gmail.com>
2024-10-17 16:47:01 +0300

    Add MenuItem and OrderItem Services

---
+++ RestaurantReservationCore/Services/MenuItemService.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationCore.Db.Repositories;
+
+namespace RestaurantReservation.Services
+{
+    public class MenuItemService
+    {
+        private readonly IRepository<MenuItem> _menuItemRepository;
+
+        public MenuItemService(IRepository<MenuItem> menuItemRepository)
+        {
+            _menuItemRepository = menuItemRepository;
+        }
+
+        public async Task<List<MenuItem>> GetAllMenuItemsAsync()
+        {
+            return await _menuItemRepository.GetAllAsync();
+        }
+
+        public async Task<MenuItem> GetMenuItemByIdAsync(int id)
+        {
+            return await _menuItemRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddMenuItemAsync(MenuItem menuItem)
+        {
+            await _menuItemRepository.AddAsync(menuItem);
+        }
+
+        public async Task UpdateMenuItemAsync(MenuItem menuItem)
+        {
+            await _menuItemRepository.UpdateAsync(menuItem);
+        }
+
+        public async Task DeleteMenuItemAsync(int id)
+        {
+            var menuItem = await _menuItemRepository.GetByIdAsync(id);
+            if (menuItem == null)
+            {
+                Console.WriteLine("Menu item doesn't exist");
+            }
+            await _menuItemRepository.DeleteAsync(menuItem);
+        }
+    }
+}
---
+++ RestaurantReservationCore/Services/OrderItemService.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationCore.Db.Repositories;
+
+namespace RestaurantReservation.Services
+{
+    public class OrderItemService
+    {
+        private readonly IRepository<OrderItem> _orderItemRepository;
+
+        public OrderItemService(IRepository<OrderItem> orderItemRepository)
+        {
+            _orderItemRepository = orderItemRepository;
+        }
+
+        public async Task<List<OrderItem>> GetAllOrderItemsAsync()
+        {
+            return await _orderItemRepository.GetAllAsync();
+        }
+
+        public async Task<OrderItem> GetOrderItemByIdAsync(int id)
+        {
+            return await _orderItemRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddOrderItemAsync(OrderItem orderItem)
+        {
+            await _orderItemRepository.AddAsync(orderItem);
+        }
+
+        public async Task UpdateOrderItemAsync(OrderItem orderItem)
+        {
+            await _orderItemRepository.UpdateAsync(orderItem);
+        }
+
+        public async Task DeleteOrderItemAsync(int id)
+        {
+            var orderItem = await _orderItemRepository.GetByIdAsync(id);
+            if (orderItem == null)
+            {
+                Console.WriteLine("Order item doesn't exist");
+            }
+            await _orderItemRepository.DeleteAsync(orderItem);
+        }
+    }
+}
