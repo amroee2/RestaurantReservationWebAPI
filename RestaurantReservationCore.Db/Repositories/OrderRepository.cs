17d5322fc0f4c355ae26b04cf5be2bb830314e3c
Amro <amroqadadha@gmail.com>
2024-10-17 16:24:06 +0300

    Add Order and Restaurant Repositories

---
+++ RestaurantReservationCore.Db/Repositories/OrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservationCore.Db.DataModels;
+
+namespace RestaurantReservationCore.Db.Repositories
+{
+    public class OrderRepository : IRepository<Order>
+    {
+        private readonly RestaurantReservationDbContext _context;
+        public OrderRepository(RestaurantReservationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetAllAsync()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order> GetByIdAsync(int id)
+        {
+            return await _context.Orders.FirstOrDefaultAsync(o => o.OrderId == id);
+        }
+
+        public async Task AddAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Order order)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ RestaurantReservationCore.Db/Repositories/RestaurantRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservationCore.Db.DataModels;
+
+namespace RestaurantReservationCore.Db.Repositories
+{
+    public class RestaurantRepository : IRepository<Restaurant>
+    {
+        private readonly RestaurantReservationDbContext _context;
+        public RestaurantRepository(RestaurantReservationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Restaurant>> GetAllAsync()
+        {
+            return await _context.Restaurants.ToListAsync();
+        }
+
+        public async Task<Restaurant> GetByIdAsync(int id)
+        {
+            return await _context.Restaurants.FirstOrDefaultAsync(r => r.RestaurantId == id);
+        }
+
+        public async Task AddAsync(Restaurant restaurant)
+        {
+            await _context.Restaurants.AddAsync(restaurant);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Restaurant restaurant)
+        {
+            _context.Restaurants.Update(restaurant);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Restaurant restaurant)
+        {
+            _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
