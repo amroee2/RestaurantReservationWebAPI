5596e2d6f869c2be02db8b8af8aa34dbf416039f
Amro <amroqadadha@gmail.com>
2024-10-17 20:25:54 +0300

    Implement Order Service

--- RestaurantReservationCore/Services/OrderService.cs
+++ RestaurantReservationCore/Services/OrderService.cs
@@ -12,14 +12,28 @@ namespace RestaurantReservationCore.Services
             _orderRepository = orderRepository;
         }
 
-        public async Task<List<Order>> GetAllOrdersAsync()
+        public async Task GetAllOrdersAsync()
         {
-            return await _orderRepository.GetAllAsync();
+            List<Order> orders =  await _orderRepository.GetAllAsync();
+            if (!orders.Any())
+            {
+                Console.WriteLine("No orders found");
+                return;
+            }
+            foreach (var order in orders) {
+                Console.WriteLine(order);
+            }
         }
 
-        public async Task<Order> GetOrderByIdAsync(int id)
+        public async Task GetOrderByIdAsync(int id)
         {
-            return await _orderRepository.GetByIdAsync(id);
+            Order order =  await _orderRepository.GetByIdAsync(id);
+            if (order == null)
+            {
+                Console.WriteLine("Order not found");
+                return;
+            }
+            Console.WriteLine(order);
         }
 
         public async Task AddOrderAsync(Order order)
@@ -27,9 +41,19 @@ namespace RestaurantReservationCore.Services
             await _orderRepository.AddAsync(order);
         }
 
-        public async Task UpdateOrderAsync(Order order)
+        public async Task UpdateOrderAsync(int id, Order order)
         {
-            await _orderRepository.UpdateAsync(order);
+            Order updatedOrder = await _orderRepository.GetByIdAsync(id);
+            if (updatedOrder == null)
+            {
+                Console.WriteLine("Customer not found");
+                return;
+            }
+            updatedOrder.OrderDate = order.OrderDate;
+            updatedOrder.TotalAmount = order.TotalAmount;
+            updatedOrder.ReservationId = order.ReservationId;
+            updatedOrder.EmployeeId = order.EmployeeId;
+            await _orderRepository.UpdateAsync(updatedOrder);
         }
 
         public async Task DeleteOrderAsync(int id)
