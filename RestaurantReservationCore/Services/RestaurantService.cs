15b8424d396ae13caca5a8b2b10360e6cce74aea
Amro <amroqadadha@gmail.com>
2024-10-17 16:48:19 +0300

    Add Restaurant and Table Services

---
+++ RestaurantReservationCore/Services/RestaurantService.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationCore.Db.Repositories;
+
+namespace RestaurantReservation.Services
+{
+    public class RestaurantService
+    {
+        private readonly IRepository<Restaurant> _restaurantRepository;
+
+        public RestaurantService(IRepository<Restaurant> restaurantRepository)
+        {
+            _restaurantRepository = restaurantRepository;
+        }
+
+        public async Task<List<Restaurant>> GetAllRestaurantsAsync()
+        {
+            return await _restaurantRepository.GetAllAsync();
+        }
+
+        public async Task<Restaurant> GetRestaurantByIdAsync(int id)
+        {
+            return await _restaurantRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddRestaurantAsync(Restaurant restaurant)
+        {
+            await _restaurantRepository.AddAsync(restaurant);
+        }
+
+        public async Task UpdateRestaurantAsync(Restaurant restaurant)
+        {
+            await _restaurantRepository.UpdateAsync(restaurant);
+        }
+
+        public async Task DeleteRestaurantAsync(int id)
+        {
+            var restaurant = await _restaurantRepository.GetByIdAsync(id);
+            if (restaurant == null)
+            {
+                Console.WriteLine("Restaurant doesn't exist");
+            }
+            await _restaurantRepository.DeleteAsync(restaurant);
+        }
+    }
+}
---
+++ RestaurantReservationCore/Services/TableService.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationCore.Db.Repositories;
+
+namespace RestaurantReservation.Services
+{
+    public class TableService
+    {
+        private readonly IRepository<Table> _tableRepository;
+
+        public TableService(IRepository<Table> tableRepository)
+        {
+            _tableRepository = tableRepository;
+        }
+
+        public async Task<List<Table>> GetAllTablesAsync()
+        {
+            return await _tableRepository.GetAllAsync();
+        }
+
+        public async Task<Table> GetTableByIdAsync(int id)
+        {
+            return await _tableRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddTableAsync(Table table)
+        {
+            await _tableRepository.AddAsync(table);
+        }
+
+        public async Task UpdateTableAsync(Table table)
+        {
+            await _tableRepository.UpdateAsync(table);
+        }
+
+        public async Task DeleteTableAsync(int id)
+        {
+            var table = await _tableRepository.GetByIdAsync(id);
+            if (table == null)
+            {
+                Console.WriteLine("Table doesn't exist");
+            }
+            await _tableRepository.DeleteAsync(table);
+        }
+    }
+}
