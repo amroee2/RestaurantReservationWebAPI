e7c5a89a04adb3bd6db01f559d775117b381ee48
Amro <amroqadadha@gmail.com>
2024-10-17 16:21:21 +0300

    Add Customer Repository and Repository Interface

---
+++ RestaurantReservationCore.Db/Repositories/CustomerRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservationCore.Db.DataModels;
+
+namespace RestaurantReservationCore.Db.Repositories
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        private readonly RestaurantReservationDbContext _context;
+        public CustomerRepository(RestaurantReservationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Customer>> GetAllAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+        }
+
+        public async Task AddAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ RestaurantReservationCore.Db/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservationCore.Db.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
