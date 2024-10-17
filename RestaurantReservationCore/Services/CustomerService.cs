271e74860cbf4bca32d9844dc43c8854be25d69b
Amro <amroqadadha@gmail.com>
2024-10-17 16:44:52 +0300

    Add Customer and Employee Services

---
+++ RestaurantReservationCore/Services/CustomerService.cs
@@ -0,0 +1,72 @@
+﻿using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationCore.Db.Repositories;
+
+namespace RestaurantReservation.Services
+{
+    public class CustomerService
+    {
+
+        private readonly IRepository<Customer> _customerRepository;
+
+        public CustomerService(IRepository<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task GetAllCustomersAsync()
+        {
+            List<Customer> customers =  await _customerRepository.GetAllAsync();
+            if(customers.Count == 0)
+            {
+                Console.WriteLine("No customers found");
+                return;
+            }
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+        }
+
+        public async Task GetCustomerByIdAsync(int id)
+        {
+            Customer customer = await _customerRepository.GetByIdAsync(id);
+            if (customer == null)
+            {
+                Console.WriteLine("Customer not found");
+                return;
+            }
+            Console.WriteLine(customer);
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            await _customerRepository.AddAsync(customer);
+        }
+
+        public async Task UpdateCustomerAsync(int Id, Customer customer)
+        {
+            Customer updatedCustomer = await _customerRepository.GetByIdAsync(Id);
+            if (updatedCustomer == null)
+            {
+                Console.WriteLine("Customer not found");
+                return;
+            }
+            updatedCustomer.PhoneNumber = customer.PhoneNumber;
+            updatedCustomer.Email = customer.Email;
+            updatedCustomer.FirstName = customer.FirstName;
+            updatedCustomer.LastName = customer.LastName;
+            await _customerRepository.UpdateAsync(updatedCustomer);
+        }
+
+        public async Task DeleteCustomerAsync(int id)
+        {
+            Customer customer = await _customerRepository.GetByIdAsync(id);
+            if (customer == null)
+            {
+                Console.WriteLine("Customer not found");
+                return;
+            }
+            await _customerRepository.DeleteAsync(customer);
+        }
+    }
+}
---
+++ RestaurantReservationCore/Services/EmployeeService.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationCore.Db.Repositories;
+
+namespace RestaurantReservation.Services
+{
+    public class EmployeeService
+    {
+        private readonly IRepository<Employee> _employeeRepository;
+
+        public EmployeeService(IRepository<Employee> employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<List<Employee>> GetAllEmployeesAsync()
+        {
+            return await _employeeRepository.GetAllAsync();
+        }
+
+        public async Task<Employee> GetEmployeeByIdAsync(int id)
+        {
+            return await _employeeRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            await _employeeRepository.AddAsync(employee);
+        }
+
+        public async Task UpdateEmployeeAsync(Employee employee)
+        {
+            await _employeeRepository.UpdateAsync(employee);
+        }
+
+        public async Task DeleteEmployeeAsync(int id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+            if(employee == null)
+            {
+                Console.WriteLine("Employee doesn't exist");
+            }
+            await _employeeRepository.DeleteAsync(employee);
+        }
+    }
+}
