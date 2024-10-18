78a20e5eba66c75527070df880ae57ccb1e6ae24
Amro <amroqadadha@gmail.com>
2024-10-18 18:13:38 +0300

    Add Customer Reservations With Restaurant Name view

---
+++ RestaurantReservationCore.Db/Views/CustomerReservationsByRestaurant.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantReservationCore.Db.Views
+{
+    public class CustomerReservationsByRestaurant
+    {
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+
+        [Column("Name")]
+        public string RestaurantName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{CustomerName}, {ReservationDate}, {RestaurantName}";
+        }
+    }
+}
