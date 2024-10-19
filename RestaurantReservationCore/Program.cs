000ff884d83bb24122dd866984cee76f817b0687
Amro <amroqadadha@gmail.com>
2024-10-19 13:07:35 +0300

    Keep program running continuously until user chooses to exit

--- RestaurantReservationCore/Program.cs
+++ RestaurantReservationCore/Program.cs
@@ -4,57 +4,64 @@ using RestaurantReservationCore.Enums;
 using RestaurantReservationCore.Services;
 using RestaurantReservationCore.UI;
 
-Console.WriteLine("Welcome to the system!");
-Console.WriteLine("1- Customer\n2- Employee\n3- Menu Item\n4- Reservation\n5- Order\n" +
-    "6- Order Item\n7- Restaurant\n8- Table\n ");
+while (true)
+{
+    Console.WriteLine("Welcome to the system!");
+    Console.WriteLine("1- Customer\n2- Employee\n3- Menu Item\n4- Reservation\n5- Order\n" +
+        "6- Order Item\n7- Restaurant\n8- Table\n0- Exit");
 
-try
-{
-    Enum.TryParse(Console.ReadLine(), out EntityOptions option);
-    HandleRequest(option);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-static void HandleRequest(EntityOptions option)
-{
-    RestaurantReservationDbContext restaurantReservationDbContext = new RestaurantReservationDbContext();
-    switch (option)
+    try
     {
-        case EntityOptions.Customer:
-            CustomerUI customerUI = new CustomerUI(new CustomerService(new CustomerRepository(restaurantReservationDbContext)));
-            customerUI.DisplayOptions();
-            break;
-        case EntityOptions.Employee:
-            EmployeeUI employeeUI = new EmployeeUI(new EmployeeService(new EmployeeRepository(restaurantReservationDbContext)));
-            employeeUI.DisplayOptions();
-            break;
-        case EntityOptions.MenuItem:
-            MenuItemUI menuItemUI = new MenuItemUI(new MenuItemService(new MenuItemRepository(restaurantReservationDbContext)));
-            menuItemUI.DisplayOptions();
-            break;
-        case EntityOptions.OrderItem:
-            OrderItemUI orderItemUI = new OrderItemUI(new OrderItemService(new OrderItemRepository(restaurantReservationDbContext)));
-            orderItemUI.DisplayOptions();
-            break;
-        case EntityOptions.Order:
-            OrderUI orderUI = new OrderUI(new OrderService(new OrderRepository(restaurantReservationDbContext)));
-            orderUI.DisplayOptions();
-            break;
-        case EntityOptions.Reservation:
-            ReservationUI reservationUI = new ReservationUI(new ReservationService(new ReservationRepository(restaurantReservationDbContext)));
-            reservationUI.DisplayOptions();
-            break;
-        case EntityOptions.Restaurant:
-            RestaurantUI restaurantUI = new RestaurantUI(new RestaurantService(new RestaurantRepository(restaurantReservationDbContext)));
-            restaurantUI.DisplayOptions();
-            break;
-        case EntityOptions.Table:
-            TableUI tableUI = new TableUI(new TableService(new TableRepository(restaurantReservationDbContext)));
-            tableUI.DisplayOptions();
-            break;
+        Enum.TryParse(Console.ReadLine(), out EntityOptions option);
+        if (option == EntityOptions.Exit)
+        {
+            return;
+        }
+        HandleRequest(option);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    static void HandleRequest(EntityOptions option)
+    {
+        RestaurantReservationDbContext restaurantReservationDbContext = new RestaurantReservationDbContext();
+        switch (option)
+        {
+            case EntityOptions.Customer:
+                CustomerUI customerUI = new CustomerUI(new CustomerService(new CustomerRepository(restaurantReservationDbContext)));
+                customerUI.DisplayOptions();
+                break;
+            case EntityOptions.Employee:
+                EmployeeUI employeeUI = new EmployeeUI(new EmployeeService(new EmployeeRepository(restaurantReservationDbContext)));
+                employeeUI.DisplayOptions();
+                break;
+            case EntityOptions.MenuItem:
+                MenuItemUI menuItemUI = new MenuItemUI(new MenuItemService(new MenuItemRepository(restaurantReservationDbContext)));
+                menuItemUI.DisplayOptions();
+                break;
+            case EntityOptions.OrderItem:
+                OrderItemUI orderItemUI = new OrderItemUI(new OrderItemService(new OrderItemRepository(restaurantReservationDbContext)));
+                orderItemUI.DisplayOptions();
+                break;
+            case EntityOptions.Order:
+                OrderUI orderUI = new OrderUI(new OrderService(new OrderRepository(restaurantReservationDbContext)));
+                orderUI.DisplayOptions();
+                break;
+            case EntityOptions.Reservation:
+                ReservationUI reservationUI = new ReservationUI(new ReservationService(new ReservationRepository(restaurantReservationDbContext)));
+                reservationUI.DisplayOptions();
+                break;
+            case EntityOptions.Restaurant:
+                RestaurantUI restaurantUI = new RestaurantUI(new RestaurantService(new RestaurantRepository(restaurantReservationDbContext)));
+                restaurantUI.DisplayOptions();
+                break;
+            case EntityOptions.Table:
+                TableUI tableUI = new TableUI(new TableService(new TableRepository(restaurantReservationDbContext)));
+                tableUI.DisplayOptions();
+                break;
+        }
     }
 }
 
