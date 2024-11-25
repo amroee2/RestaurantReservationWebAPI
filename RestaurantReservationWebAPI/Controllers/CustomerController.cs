413de0c989144d829f15130bf5d7ac4d1dde2fa8
Amro <amroqadadha@gmail.com>
2024-11-25 17:09:10 +0200

    Create Customer Controller

---
+++ RestaurantReservationWebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservationServices.Services.CustomerManagementService;
+
+namespace RestaurantReservationWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet("Customers")]
+        public async Task<ActionResult> GetAllCustomers()
+        {
+            return Ok(await _customerService.GetAllCustomersAsync());
+        }
+    }
+}
