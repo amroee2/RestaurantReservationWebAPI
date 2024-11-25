9cba76a6a7e39c9c7bd3a0a39b164b48a321a43a
Amro <amroqadadha@gmail.com>
2024-11-25 22:20:27 +0200

    Create restaurant dtos mapper

---
+++ RestaurantReservationServices/Mappings/RestaurantProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationServices.DTOs.RestaurantDTOs;
+
+namespace RestaurantReservationServices.Mappings
+{
+    public class RestaurantProfile: Profile
+    {
+        public RestaurantProfile()
+        {
+            CreateMap<Restaurant, RestaurantReadDTO>();
+            CreateMap<RestaurantCreateDTO, Restaurant>();
+            CreateMap<RestaurantUpdateDTO, Restaurant>();
+        }
+    }
+}
