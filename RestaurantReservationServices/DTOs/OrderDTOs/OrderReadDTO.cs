0175c1743af167352f059d0f9b0e53e98aec77db
Amro <amroqadadha@gmail.com>
2024-11-26 19:37:13 +0200

    Create OrderDTOS

---
+++ RestaurantReservationServices/DTOs/OrderDTOs/OrderReadDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservationServices.DTOs.OrderDTOs
+{
+    public class OrderReadDTO
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double TotalAmount { get; set; }
+        public int EmployeeId { get; set; }
+        public int ReservationId { get; set; }
+    }
+}
