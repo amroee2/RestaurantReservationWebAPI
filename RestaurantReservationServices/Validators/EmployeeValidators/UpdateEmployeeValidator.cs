884871c2ef867f2744a4be828a638a4d7d04209c
Amro <amroqadadha@gmail.com>
2024-11-26 15:08:21 +0200

    Implement Employee validators

---
+++ RestaurantReservationServices/Validators/EmployeeValidators/UpdateEmployeeValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using RestaurantReservationServices.DTOs.EmployeeDTOs;
+
+namespace RestaurantReservationServices.Validators.EmployeeValidators
+{
+    public class UpdateEmployeeValidator: AbstractValidator<EmployeeUpdateDTO>
+    {
+        public UpdateEmployeeValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("First name is required")
+                .MaximumLength(50)
+                .WithMessage("First name can't be longer than 50 characters");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Last name is required")
+                .MaximumLength(50)
+                .WithMessage("Last name can't be longer than 50 characters");
+
+            RuleFor(x => x.Position)
+                .NotEmpty()
+                .WithMessage("Position is required")
+                .MaximumLength(50)
+                .WithMessage("Position can't be longer than 50 characters");
+
+            RuleFor(x => x.RestaurantId)
+                .NotEmpty()
+                .WithMessage("Restaurant id is required")
+                .GreaterThan(0);
+        }
+    }
+}
