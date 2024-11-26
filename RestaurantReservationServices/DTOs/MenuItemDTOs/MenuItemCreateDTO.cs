7c994c64e8cd86fc527218f9e2d9b0be0618f500
Amro <amroqadadha@gmail.com>
2024-11-26 15:52:59 +0200

    Create MenuItemDTOs

---
+++ RestaurantReservationServices/DTOs/MenuItemDTOs/MenuItemCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservationServices.DTOs.MenuItemDTOs
+{
+    public class MenuItemCreateDTO
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
---
+++ RestaurantReservationServices/DTOs/MenuItemDTOs/MenuItemReadDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservationServices.DTOs.MenuItemDTOs
+{
+    public class MenuItemReadDTO
+    {
+        public int MenuItemId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
