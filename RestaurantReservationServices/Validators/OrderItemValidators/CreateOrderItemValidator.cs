c1bc61ab08905c943cfb62b4ca5629b20ec03250
Amro <amroqadadha@gmail.com>
2024-11-26 20:07:46 +0200

    Create OrderItem Validators

---
+++ RestaurantReservationServices/Validators/OrderItemValidators/CreateOrderItemValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using RestaurantReservationServices.DTOs.OrderItemDTOs;
+
+namespace RestaurantReservationServices.Validators.OrderItemValidators
+{
+    public class CreateOrderItemValidator: AbstractValidator<OrderItemCreateDTO>
+    {
+        public CreateOrderItemValidator()
+        {
+            RuleFor(x => x.OrderId)
+                .NotEmpty()
+                .WithMessage("Order Id is required")
+                .GreaterThan(0)
+                .WithMessage("Order Id must be greater than 0");
+            RuleFor(x => x.MenuItemId)
+                .NotEmpty()
+                .WithMessage("Menu Item Id is required")
+                .GreaterThan(0)
+                .WithMessage("Menu Item Id must be greater than 0");
+            RuleFor(x => x.Quantity)
+                .NotEmpty()
+                .WithMessage("Quantity is required")
+                .GreaterThan(0)
+                .WithMessage("Quantity must be greater than 0");
+        }
+    }
+}
