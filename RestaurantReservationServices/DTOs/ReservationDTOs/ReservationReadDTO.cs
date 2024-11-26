7379f10a65cc74db1f27104c2a85358dcde51d31
Amro <amroqadadha@gmail.com>
2024-11-26 18:42:51 +0200

    Create ReservationDTOs

---
+++ RestaurantReservationServices/DTOs/ReservationDTOs/ReservationReadDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantReservationServices.DTOs.ReservationDTOs
+{
+    public class ReservationReadDTO
+    {
+        public int ReservationId { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public int PartySize { get; set; }
+        public int CustomerId { get; set; }
+        public int TableId { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
---
+++ RestaurantReservationServices/DTOs/ReservationDTOs/ReservationUpdateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservationServices.DTOs.ReservationDTOs
+{
+    public class ReservationUpdateDTO
+    {
+        public DateTime ReservationDate { get; set; }
+        public int PartySize { get; set; }
+        public int CustomerId { get; set; }
+        public int TableId { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
