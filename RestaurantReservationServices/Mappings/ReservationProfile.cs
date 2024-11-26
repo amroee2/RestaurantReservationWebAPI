2575de7287764327920ffd98b981a79f1ceb3999
Amro <amroqadadha@gmail.com>
2024-11-26 18:43:01 +0200

    Map ReservationDTOs

---
+++ RestaurantReservationServices/Mappings/ReservationProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using RestaurantReservationCore.Db.DataModels;
+using RestaurantReservationServices.DTOs.ReservationDTOs;
+
+namespace RestaurantReservationServices.Mappings
+{
+    public class ReservationProfile: Profile
+    {
+        public ReservationProfile()
+        {
+            CreateMap<Reservation, ReservationReadDTO>();
+            CreateMap<ReservationCreateDTO, Reservation>();
+            CreateMap<ReservationUpdateDTO, Reservation>();
+            CreateMap<ReservationReadDTO, ReservationUpdateDTO>();
+        }
+    }
+}
